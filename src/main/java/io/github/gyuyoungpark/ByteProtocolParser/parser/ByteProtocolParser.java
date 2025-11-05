948f03a4ff3b8070fc1a4ebbb123160d3b0a33d2
gyu-young-park <gyoue200125@gmail.com>
2025-11-05 23:44:39 +0900

    feat: add byte protocol parser

--- src/main/java/io/github/gyuyoungpark/ByteProtocolParser/parser/ByteProtocolParser.java
+++ src/main/java/io/github/gyuyoungpark/ByteProtocolParser/parser/ByteProtocolParser.java
@@ -1,10 +1,30 @@
 package io.github.gyuyoungpark.ByteProtocolParser.parser;
 
-import io.github.gyuyoungpark.data.TransactionData;
+import io.github.gyuyoungpark.ByteProtocolParser.annotation.ByteProtocolField;
+import io.github.gyuyoungpark.ByteProtocolParser.annotation.ByteProtocolMessage;
+
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
 
 public class ByteProtocolParser implements IByteProtocolParser {
-    public TransactionData Parse() {
+    public static void Parse(byte[] bytes, Object instance) {
+        Class<?> clazz = instance.getClass();
+        ByteProtocolMessage byteProtocolMessage = clazz.getAnnotation(ByteProtocolMessage.class);
+        if (byteProtocolMessage == null) throw new IllegalArgumentException(clazz.getName() + " does not have byte protocol message annotation");
 
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);;
+        for (Field field: clazz.getFields()) {
+            ByteProtocolField byteProtocolField = field.getAnnotation(ByteProtocolField.class);
+            if (byteProtocolField == null) continue;
+            field.setAccessible(true);
+
+            int start = byteProtocolField.start();
+            int length = byteProtocolField.length();
+            byte[] buffer = new byte[length];
+            byteBuffer.get(buffer, start, length);
+
+
+        }
     }
 
 }
